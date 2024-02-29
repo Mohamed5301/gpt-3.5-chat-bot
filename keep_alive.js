9a9fe1cb53c36d84d235c53910e25131c1e1f1e0
Mohamed <160239089+Mohamed5301@users.noreply.github.com>
2024-02-29 18:13:46 +0200

    keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js'); //import client from discord
+const keep_alive = require('./keep_alive.js')
+const client = new Discord.Client();
 
+server.all("/", (req, res) => {
+  res.send("Bot is running!")
+})
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Server is ready.")
+  })
+}
+
+module.exports = keepAlive
